5acaca85c04936f572e76db4f5840a630c43c8dd
Rune K. Svendsen <runesvend@gmail.com>
2018-10-18 10:39:34 +0200

    Create CryptoDepth.Types

---
+++ src/CryptoDepth/Types.hs
@@ -0,0 +1,12 @@
+module CryptoDepth.Types
+( Sym, Venue, SymVenue
+, module Amount
+, module EdgePath
+, module OneDiv
+)
+where
+
+import CryptoDepth.Internal.Types           (Sym, Venue, SymVenue)
+import CryptoDepth.Internal.Types.Amount    as Amount
+import CryptoDepth.Internal.Types.EdgePath  as EdgePath
+import CryptoDepth.Internal.Types.OneDiv    as OneDiv
